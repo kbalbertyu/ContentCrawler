9a135fdd879124192e40a66c0d5265bc3786b730
Albert Yu <kbalbertyu@gmail.com>
2019-01-11 11:12:02 +0800

    Add DownloadResult model

---
+++ src/main/java/cn/btimes/model/DownloadResult.java
@@ -0,0 +1,15 @@
+package cn.btimes.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author <a href="mailto:kbalbertyu@gmail.com">Albert Yu</a> 2019-01-10 8:21 PM
+ */
+@Data
+@AllArgsConstructor
+public class DownloadResult {
+    private String url;
+    private String fullPath;
+    private String fileName;
+}
