9f962fdea65495687ecb66113ebc799e63296c7b
Albert Yu <kbalbertyu@gmail.com>
2019-04-22 15:22:08 +0800

    Determine date from article page in JieMian

--- src/main/java/cn/btimes/source/JieMian.java
+++ src/main/java/cn/btimes/source/JieMian.java
@@ -1,12 +1,16 @@
 package cn.btimes.source;
 
 import cn.btimes.model.common.Article;
+import cn.btimes.model.common.BTExceptions.PastDateException;
 import cn.btimes.model.common.CSSQuery;
 import cn.btimes.model.common.Category;
+import com.amzass.service.sellerhunt.HtmlParser;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
@@ -14,6 +18,7 @@ import java.util.*;
  * @author <a href="mailto:kbalbertyu@gmail.com">Albert Yu</a> 2019-01-04 7:10 AM
  */
 public class JieMian extends SourceWithoutDriver {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private static final Map<String, Category> URLS = new HashMap<>();
 
     static {
@@ -31,36 +36,45 @@ public class JieMian extends SourceWithoutDriver {
 
     @Override
     protected String getDateRegex() {
-        return null;
+        return "\\d{4}/\\d{2}/\\d{2} \\d{2}:\\d{2}";
     }
 
     @Override
     protected String getDateFormat() {
-        return null;
+        return "yyyy/MM/dd HH:mm";
     }
 
     @Override
     protected CSSQuery getCSSQuery() {
         return new CSSQuery(".news-list > .list-view > .news-view", ".article-main", "h3 > a",
-            ".article-header > p", ".article-source > p:contains(来源：)", ".date");
+            ".article-header > p", ".article-source > p:contains(来源：)", ".article-info");
     }
 
     @Override
     protected List<Article> parseList(Document doc) {
         List<Article> articles = new ArrayList<>();
         Elements list = this.readList(doc);
-        this.parseDateTitleList(articles, list);
+        for (Element row : list) {
+            try {
+                Article article = new Article();
+                this.checkDateTextExistence(row, ".date");
+                String timeText = HtmlParser.text(row, ".date");
+                this.parseDescribableDateText(timeText);
+
+                this.parseTitle(row, article);
+                articles.add(article);
+            } catch (PastDateException e) {
+                logger.warn("Article that past {} minutes detected, complete the list fetching: ",
+                    config.getMaxPastMinutes(), e);
+                break;
+            }
+        }
         return articles;
     }
 
     @Override
-    protected Date parseDateText(String timeText) {
-        return this.parseDescribableDateText(timeText);
-    }
-
-    @Override
     protected void readArticle(WebDriver driver, Article article) {
-        this.readSummaryContent(driver, article);
+        this.readDateSummaryContent(driver, article);
     }
 
     @Override
