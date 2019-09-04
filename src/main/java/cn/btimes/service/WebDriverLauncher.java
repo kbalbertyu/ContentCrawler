554996b2ef878cb7bfcbe0547948df17316ca400
Albert Yu <kbalbertyu@gmail.com>
2019-09-04 14:59:19 +0800

    Add captcha resolving in admin login

--- src/main/java/cn/btimes/service/WebDriverLauncher.java
+++ src/main/java/cn/btimes/service/WebDriverLauncher.java
@@ -6,18 +6,22 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.amzass.enums.common.ConfigEnums.ChromeDriverVersion;
 import com.amzass.enums.common.Directory;
+import com.amzass.service.ocr.CaptchaResolver;
+import com.amzass.service.ocr.CaptchaResolver.Handler;
 import com.amzass.utils.PageLoadHelper;
 import com.amzass.utils.PageLoadHelper.WaitTime;
 import com.amzass.utils.common.Constants;
+import com.amzass.utils.common.Exceptions.BusinessException;
 import com.amzass.utils.common.PageUtils;
 import com.amzass.utils.common.Tools;
 import com.google.inject.Inject;
 import org.apache.commons.lang3.StringUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.HashMap;
@@ -27,6 +31,7 @@ import java.util.Map;
  * @author <a href="mailto:kbalbertyu@gmail.com">Albert Yu</a> 2018-12-29 2:37 PM
  */
 public class WebDriverLauncher {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     static final String DOWNLOAD_PATH = System.getProperty("user.dir") + "/downloads";
     private static final boolean USE_HEADLESS_DRIVER = StringUtils.isNotBlank(Tools.getCustomizingValue("USE_HEADLESS_DRIVER"));
     private static final boolean DISABLE_JS_IMG_ADS = false;
@@ -104,10 +109,60 @@ public class WebDriverLauncher {
 
         PageUtils.setValue(driver, By.id("mb_email"), config.getAdminEmail());
         PageUtils.setValue(driver, By.id("login_mb_password"), config.getAdminPassword());
-        PageUtils.click(driver, By.cssSelector("button[type=submit]"));
-        WaitTime.Normal.execute();
 
-        this.loadCookies(driver, config, cookieFile);
+        try {
+            PageUtils.click(driver, By.cssSelector("button[type=submit]"));
+            WaitTime.Normal.execute();
+            this.loadCookies(driver, config, cookieFile);
+        } catch (UnhandledAlertException e) {
+            this.resetPassword(driver, config);
+            this.loadCookies(driver, config, cookieFile);
+        }
+    }
+
+    @Inject private CaptchaResolver captchaResolver;
+
+    private void resetPassword(WebDriver driver, Config config) {
+        Alert alert = driver.switchTo().alert();
+        if (alert != null) {
+            alert.accept();
+        }
+        if (!PageLoadHelper.present(driver, By.name("mb_password"), WaitTime.Short)) {
+            return;
+        }
+        PageUtils.setValue(driver, By.name("mb_password"), config.getAdminPassword());
+        PageUtils.setValue(driver, By.name("mb_password_re"), config.getAdminPassword());
+        String code = this.resolveCaptcha(config);
+        if (StringUtils.isNotBlank(code)) {
+            PageUtils.setValue(driver, By.name("wr_key"), code);
+        }
+        PageUtils.click(driver, By.cssSelector("button[type=submit]"));
+        WaitTime.Short.execute();
+    }
+
+    private String resolveCaptcha(Config config) {
+        String captchaSrc = config.getAdminUrl() + "/plugin/captcha/";
+        Handler[] handlers = captchaResolver.getHandlers();
+        String code = null;
+        for (Handler handler : handlers) {
+            try {
+                code = handler.process(captchaSrc, null);
+                if (StringUtils.isNotBlank(code)) {
+                    break;
+                }
+            } catch (WebDriverException e) {
+                // 如果是WebDriver异常, 重复处理实际无效, 终止
+                logger.error("通过{}处理验证码过程中出现终结性WebDriver异常:", handler.id(), e);
+                break;
+            } catch (Exception e) {
+                logger.error("尝试通过{}处理验证码过程中出现异常:", handler.id(), e);
+            }
+            WaitTime.Normal.execute();
+        }
+        if (StringUtils.isBlank(code)) {
+            throw new BusinessException("Unable to parse captcha code.");
+        }
+        return code;
     }
 
     private void loadCookies(WebDriver driver, Config config, File cookieFile) {
