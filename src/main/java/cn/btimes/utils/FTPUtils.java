14e227cc11b4880c39058dc9a3ce516aa3b5262b
Albert Yu <kbalbertyu@gmail.com>
2019-10-14 22:49:33 +0800

    Add logger to FTPUtils

--- src/main/java/cn/btimes/utils/FTPUtils.java
+++ src/main/java/cn/btimes/utils/FTPUtils.java
@@ -12,38 +12,32 @@ import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 /**
- * 支持断点续传的FTP实用类
- *
- * @version 0.3 实现中文目录创建及中文文件创建，添加对于中文的支持
+ * FTP file downloader
+ * Support resume download from break point
  */
 public class FTPUtils {
+    private final Logger logger = LoggerFactory.getLogger(FTPUtils.class);
     private static final String DB_FILE_NAME = "db.sql.gz";
     private static final String DB_BACKUP_FILE_NAME = "db_bk.sql.gz";
     private static final String DOWNLOAD_PATH = "downloads";
     @Inject private FTPClient ftpClient;
 
-    /**
-     * 枚举类DownloadStatus代码
-     */
     public enum DownloadStatus {
-        Remote_File_Not_Exist, //远程文件不存在   
-        Local_Bigger_Remote, //本地文件大于远程文件   
-        Download_From_Break_Success, //断点下载文件成功   
-        Download_From_Break_Failed,   //断点下载文件失败   
-        Download_New_Success,    //全新下载文件成功   
-        Download_New_Failed    //全新下载文件失败
+        Remote_File_Not_Exist,
+        Local_Bigger_Remote,
+        Download_From_Break_Success,
+        Download_From_Break_Failed,
+        Download_New_Success,
+        Download_New_Failed
     }
 
-    /**
-     * 连接到FTP服务器
-     *
-     * @return 是否连接成功
-     */
     public boolean connect() throws IOException {
         FTPConfig ftpConfig = this.loadFTPConfig();
         ftpClient.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
@@ -52,7 +46,6 @@ public class FTPUtils {
         if (FTPReply.isPositiveCompletion(ftpClient.getReplyCode())) {
             if (ftpClient.login(ftpConfig.getUsername(), ftpConfig.getPwd())) {
                 if (ftpConfig.isActiveMode()) {
-                    //设置被动模式
                     ftpClient.enterLocalActiveMode();
                 } else {
                     ftpClient.enterLocalPassiveMode();
@@ -73,38 +66,27 @@ public class FTPUtils {
         return JSONObject.parseObject(Tools.readFileToString(file), FTPConfig.class);
     }
 
-    /**
-     * 从FTP服务器上下载文件,支持断点续传，上传百分比汇报
-     *
-     * @param remote 远程文件路径
-     * @param local  本地文件路径
-     * @return 上传的状态
-     */
     public DownloadStatus download(String remote, String local) throws IOException {
-        //设置以二进制方式传输     
         ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
         DownloadStatus result;
 
-        //检查远程文件是否存在     
         FTPFile[] files = ftpClient.listFiles(new String(remote.getBytes(StandardCharsets.UTF_8)));
         if (files.length != 1) {
-            System.out.println("远程文件不存在");
+            logger.error("Remote file not exists");
             return DownloadStatus.Remote_File_Not_Exist;
         }
 
         long lRemoteSize = files[0].getSize();
         File f = new File(local);
 
-        //本地存在文件，进行断点下载     
+        // Local file exists already, resume downloading from break point
         if (f.exists()) {
             long localSize = f.length();
-            //判断本地文件大小是否大于远程文件大小     
             if (localSize >= lRemoteSize) {
-                System.out.println("本地文件大于远程文件，下载中止");
+                logger.warn("Local file larger than remote file, stop downloading.");
                 return DownloadStatus.Local_Bigger_Remote;
             }
 
-            //进行断点续传，并记录状态     
             FileOutputStream out = new FileOutputStream(f, true);
             ftpClient.setRestartOffset(localSize);
             InputStream in = ftpClient.retrieveFileStream(new String(remote.getBytes(StandardCharsets.UTF_8)));
@@ -143,7 +125,7 @@ public class FTPUtils {
             if (nowProcess > process) {
                 process = nowProcess;
                 if (process % 10 == 0) {
-                    System.out.println("下载进度：" + process);
+                    logger.info("Download progress：" + process);
                 }
             }
         }
@@ -152,9 +134,6 @@ public class FTPUtils {
         return ftpClient.completePendingCommand();
     }
 
-    /**
-     * 断开与远程服务器的连接
-     */
     private void disconnect() throws IOException {
         if (ftpClient.isConnected()) {
             ftpClient.disconnect();
@@ -171,11 +150,11 @@ public class FTPUtils {
                     FileUtils.deleteQuietly(backFile);
                 }
                 File file = FileUtils.getFile(DOWNLOAD_PATH, DB_FILE_NAME);
-                FileUtils.copyFile(file, backFile);
+                FileUtils.moveFile(file, backFile);
                 this.disconnect();
                 break;
             } catch (IOException e) {
-                System.out.println("连接FTP出错：" + e.getMessage());
+                logger.error("FTP connection error：", e);
             }
         }
     }
