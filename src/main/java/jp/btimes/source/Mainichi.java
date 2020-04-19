f0f74659565d89a71527aa8acf00c98db4ce0c2e
Albert Yu <kbalbertyu@gmail.com>
2020-04-19 22:35:18 +0800

    :construct: Add Manichi article source for BTimesJP

---
+++ src/main/java/jp/btimes/source/Mainichi.java
@@ -0,0 +1,59 @@
+package jp.btimes.source;
+
+import cn.btimes.model.common.Article;
+import cn.btimes.model.common.CSSQuery;
+import cn.btimes.model.common.Category;
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.WebDriver;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:kbalbertyu@gmail.com">Albert Yu</a> 2020/4/19 22:29
+ */
+public class Mainichi extends Source {
+    private static final Map<String, Category> URLS = new HashMap<>();
+
+    static {
+        URLS.put("https://mainichi.jp/biz/", Category.FINANCE_JP);
+        URLS.put("https://mainichi.jp/shakai/", Category.SOCIETY_JP);
+        URLS.put("https://mainichi.jp/world/", Category.INTL_SCIENCE_JP);
+    }
+
+    @Override
+    protected Map<String, Category> getUrls() {
+        return URLS;
+    }
+
+    @Override
+    protected String getDateRegex() {
+        return null;
+    }
+
+    @Override
+    protected String getDateFormat() {
+        return null;
+    }
+
+    @Override
+    protected CSSQuery getCSSQuery() {
+        return null;
+    }
+
+    @Override
+    protected int getSourceId() {
+        return 0;
+    }
+
+    @Override
+    protected List<Article> parseList(Document doc) {
+        return null;
+    }
+
+    @Override
+    protected void readArticle(WebDriver driver, Article article) {
+
+    }
+}
