d2d1097c19b3fceb133853797c04cb8b5263cc00
Albert Yu <kbalbertyu@gmail.com>
2020-04-23 00:06:06 +0800

    :construct: Add Japanmetal article source for BTimesJP

---
+++ src/main/java/jp/btimes/source/Japanmetal.java
@@ -0,0 +1,61 @@
+package jp.btimes.source;
+
+import cn.btimes.model.common.Article;
+import cn.btimes.model.common.CSSQuery;
+import cn.btimes.model.common.Category;
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.WebDriver;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:kbalbertyu@gmail.com">Albert Yu</a> 2020/4/23 0:02
+ */
+public class Japanmetal extends Source {
+    private static final Map<String, Category> URLS = new HashMap<>();
+
+    static {
+        URLS.put("https://www.japanmetal.com/cat/news-t", Category.FINANCE_JP);
+        URLS.put("https://www.japanmetal.com/cat/news-h", Category.FINANCE_JP);
+        URLS.put("https://www.japanmetal.com/cat/news-a", Category.FINANCE_JP);
+        URLS.put("https://www.japanmetal.com/cat/news-o", Category.FINANCE_JP);
+        URLS.put("https://www.japanmetal.com/cat/news-s", Category.INTL_SCIENCE_JP);
+    }
+
+    @Override
+    protected Map<String, Category> getUrls() {
+        return URLS;
+    }
+
+    @Override
+    protected String getDateRegex() {
+        return null;
+    }
+
+    @Override
+    protected String getDateFormat() {
+        return null;
+    }
+
+    @Override
+    protected CSSQuery getCSSQuery() {
+        return null;
+    }
+
+    @Override
+    protected int getSourceId() {
+        return 0;
+    }
+
+    @Override
+    protected List<Article> parseList(Document doc) {
+        return null;
+    }
+
+    @Override
+    protected void readArticle(WebDriver driver, Article article) {
+
+    }
+}
