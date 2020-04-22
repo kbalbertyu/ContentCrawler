972b2369fd3cf1e0aafe5ec7aab61b81380d3e20
Albert Yu <kbalbertyu@gmail.com>
2020-04-22 16:54:19 +0800

    :construct: Add NNN article source for BTimesJP

---
+++ src/main/java/jp/btimes/source/NNN.java
@@ -0,0 +1,59 @@
+package jp.btimes.source;
+
+import cn.btimes.model.common.Article;
+import cn.btimes.model.common.CSSQuery;
+import cn.btimes.model.common.Category;
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.WebDriver;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:kbalbertyu@gmail.com">Albert Yu</a> 2020/4/22 16:51
+ */
+public class NNN extends Source {
+    private static final Map<String, Category> URLS = new HashMap<>();
+
+    static {
+        URLS.put("https://www.nnn.co.jp/knews/economics.html", Category.FINANCE_JP);
+        URLS.put("https://www.nnn.co.jp/knews/world.html", Category.INTL_SCIENCE_JP);
+        URLS.put("https://www.nnn.co.jp/knews/sports.html", Category.GENERAL_JP);
+    }
+
+    @Override
+    protected Map<String, Category> getUrls() {
+        return URLS;
+    }
+
+    @Override
+    protected String getDateRegex() {
+        return null;
+    }
+
+    @Override
+    protected String getDateFormat() {
+        return null;
+    }
+
+    @Override
+    protected CSSQuery getCSSQuery() {
+        return null;
+    }
+
+    @Override
+    protected int getSourceId() {
+        return 0;
+    }
+
+    @Override
+    protected List<Article> parseList(Document doc) {
+        return null;
+    }
+
+    @Override
+    protected void readArticle(WebDriver driver, Article article) {
+
+    }
+}
