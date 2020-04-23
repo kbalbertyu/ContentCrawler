8dc8f56ad597b52f449761c3ccf1034dce9f0b11
Albert Yu <kbalbertyu@gmail.com>
2020-04-23 15:27:44 +0800

    :construct: Add NbShinbun article source for BTimesJP

---
+++ src/main/java/jp/btimes/source/NbShinbun.java
@@ -0,0 +1,57 @@
+package jp.btimes.source;
+
+import cn.btimes.model.common.Article;
+import cn.btimes.model.common.CSSQuery;
+import cn.btimes.model.common.Category;
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.WebDriver;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:kbalbertyu@gmail.com">Albert Yu</a> 2020/4/23 15:23
+ */
+public class NbShinbun extends Source {
+    private static final Map<String, Category> URLS = new HashMap<>();
+
+    static {
+        URLS.put("https://www.nb-shinbun.co.jp/", Category.INTL_SCIENCE_JP);
+    }
+
+    @Override
+    protected Map<String, Category> getUrls() {
+        return URLS;
+    }
+
+    @Override
+    protected String getDateRegex() {
+        return null;
+    }
+
+    @Override
+    protected String getDateFormat() {
+        return null;
+    }
+
+    @Override
+    protected CSSQuery getCSSQuery() {
+        return null;
+    }
+
+    @Override
+    protected int getSourceId() {
+        return 0;
+    }
+
+    @Override
+    protected List<Article> parseList(Document doc) {
+        return null;
+    }
+
+    @Override
+    protected void readArticle(WebDriver driver, Article article) {
+
+    }
+}
