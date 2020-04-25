b7d41ba91520365b90fde98e89fd0a82b711434a
Albert Yu <kbalbertyu@gmail.com>
2020-04-25 14:20:58 +0800

    Implement Bci article crawling

--- src/main/java/jp/btimes/service/ServiceExecutor.java
+++ src/main/java/jp/btimes/service/ServiceExecutor.java
@@ -4,6 +4,7 @@ import cn.btimes.source.Source;
 import com.amzass.service.common.ApplicationContext;
 import jp.btimes.source.Agrinews;
 import jp.btimes.source.Asahi;
+import jp.btimes.source.Bci;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +17,7 @@ public class ServiceExecutor extends cn.btimes.service.ServiceExecutor {
     @Override
     protected List<Source> getSources() {
         List<Source> sources = new ArrayList<>();
+        sources.add(ApplicationContext.getBean(Bci.class));
         sources.add(ApplicationContext.getBean(Asahi.class));
         sources.add(ApplicationContext.getBean(Agrinews.class));
         return sources;
--- src/main/java/jp/btimes/source/Bci.java
+++ src/main/java/jp/btimes/source/Bci.java
@@ -3,17 +3,18 @@ package jp.btimes.source;
 import cn.btimes.model.common.Article;
 import cn.btimes.model.common.CSSQuery;
 import cn.btimes.model.common.Category;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author <a href="mailto:kbalbertyu@gmail.com">Albert Yu</a> 2020/4/23 0:08
  */
 public class Bci extends Source {
+    private static final int MAX_PAST_DAYS = 1;
     private static final Map<String, Category> URLS = new HashMap<>();
 
     static {
@@ -27,31 +28,44 @@ public class Bci extends Source {
 
     @Override
     protected String getDateRegex() {
-        return null;
+        return "\\d{2}/\\d{2}/\\d{2}";
     }
 
     @Override
     protected String getDateFormat() {
-        return null;
+        return "yy/MM/dd";
     }
 
     @Override
     protected CSSQuery getCSSQuery() {
-        return null;
+        return new CSSQuery(".newsTopList > ul > li", "article", "a", "",
+            "", "a");
     }
 
     @Override
     protected int getSourceId() {
-        return 0;
+        return 3;
     }
 
     @Override
     protected List<Article> parseList(Document doc) {
-        return null;
+        List<Article> articles = new ArrayList<>();
+        Elements list = this.readList(doc);
+        this.parseDateTitleList(articles, list);
+        articles.forEach(article -> {
+            String title = StringUtils.removePattern(article.getTitle(), "\\('\\d{2}/\\d{2}/\\d{2}\\)");
+            article.setTitle(title);
+        });
+        return articles;
+    }
+
+    @Override
+    protected Date parseDateText(String timeText) {
+        return this.parseDateTextWithDay(timeText, this.getDateRegex(), this.getDateFormat(), MAX_PAST_DAYS);
     }
 
     @Override
     protected void readArticle(WebDriver driver, Article article) {
-
+        this.readContent(driver, article);
     }
 }
