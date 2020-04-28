d3f1dc650e0fd81610c42acd6bfa62bc53aa226a
Albert Yu <kbalbertyu@gmail.com>
2020-04-28 16:54:33 +0800

    Implement GifuNp article crawling

--- src/main/java/jp/btimes/source/GifuNp.java
+++ src/main/java/jp/btimes/source/GifuNp.java
@@ -4,8 +4,10 @@ import cn.btimes.model.common.Article;
 import cn.btimes.model.common.CSSQuery;
 import cn.btimes.model.common.Category;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,31 +36,35 @@ public class GifuNp extends Source {
 
     @Override
     protected String getDateRegex() {
-        return null;
+        return "\\d{4}年\\d{1,2}月\\d{1,2}日 \\d{1,2}:\\d{1,2}";
     }
 
     @Override
     protected String getDateFormat() {
-        return null;
+        return "yyyy'年'MM'月'dd'日' HH:mm";
     }
 
     @Override
     protected CSSQuery getCSSQuery() {
-        return null;
+        return new CSSQuery("ul.archive > li", ".entry", ".title > a", "",
+            "", ".day");
     }
 
     @Override
     protected int getSourceId() {
-        return 0;
+        return 14;
     }
 
     @Override
     protected List<Article> parseList(Document doc) {
-        return null;
+        List<Article> articles = new ArrayList<>();
+        Elements list = this.readList(doc);
+        this.parseDateTitleList(articles, list);
+        return articles;
     }
 
     @Override
     protected void readArticle(WebDriver driver, Article article) {
-
+        this.readContent(driver, article);
     }
 }
