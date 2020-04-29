e2f2768a262b488a076a9777e1a0322660d9c090
Albert Yu <kbalbertyu@gmail.com>
2020-04-29 09:45:45 +0800

    Implement Kanaloco article crawling

--- src/main/java/jp/btimes/source/Kanaloco.java
+++ src/main/java/jp/btimes/source/Kanaloco.java
@@ -1,11 +1,23 @@
 package jp.btimes.source;
 
 import cn.btimes.model.common.Article;
+import cn.btimes.model.common.BTExceptions.PastDateException;
 import cn.btimes.model.common.CSSQuery;
 import cn.btimes.model.common.Category;
+import cn.btimes.utils.PageUtils;
+import com.amzass.utils.PageLoadHelper;
+import com.amzass.utils.PageLoadHelper.WaitTime;
+import com.amzass.utils.common.Constants;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +26,7 @@ import java.util.Map;
  * @author <a href="mailto:kbalbertyu@gmail.com">Albert Yu</a> 2020/4/22 16:30
  */
 public class Kanaloco extends Source {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private static final Map<String, Category> URLS = new HashMap<>();
 
     static {
@@ -30,31 +43,62 @@ public class Kanaloco extends Source {
 
     @Override
     protected String getDateRegex() {
-        return null;
+        return "\\d{4}年\\d{1,2}月\\d{1,2}日 \\d{1,2}:\\d{1,2}";
     }
 
     @Override
     protected String getDateFormat() {
-        return null;
+        return "yyyy'年'MM'月'dd'日' HH:mm";
     }
 
     @Override
     protected CSSQuery getCSSQuery() {
-        return null;
+        return new CSSQuery(".blk-article-list03 > section", ".acms-entry", ".tltle", "",
+            "", ".info");
     }
 
     @Override
     protected int getSourceId() {
-        return 0;
+        return 24;
     }
 
     @Override
     protected List<Article> parseList(Document doc) {
-        return null;
+        List<Article> articles = new ArrayList<>();
+        Elements list = this.readList(doc);
+        int i = 0;
+        for (Element row : list) {
+            try {
+                // Skip premium articles
+                if (row.select(".lock, .adsbygoogle").size() > 0) {
+                    continue;
+                }
+                Article article = new Article();
+                this.parseTitle(row, article);
+                this.parseDate(row, article);
+                article.setUrl(row.select("a").first().attr("href"));
+                articles.add(article);
+            } catch (PastDateException e) {
+                if (i++ < Constants.MAX_REPEAT_TIMES) {
+                    continue;
+                }
+                logger.warn("Article that past {} minutes detected, complete the list fetching: ", config.getMaxPastMinutes(), e);
+                break;
+            }
+        }
+        return articles;
     }
 
     @Override
     protected void readArticle(WebDriver driver, Article article) {
-
+        driver.get(article.getUrl());
+        if (PageLoadHelper.present(driver, By.className("lazyload"), WaitTime.Normal)) {
+            PageUtils.loadLazyContent(driver);
+        } else {
+            PageUtils.scrollToBottom(driver);
+        }
+        WaitTime.Short.execute();
+        Document doc = Jsoup.parse(driver.getPageSource());
+        this.parseContent(doc, article);
     }
 }
