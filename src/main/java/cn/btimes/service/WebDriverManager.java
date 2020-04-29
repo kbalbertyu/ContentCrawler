d82eff24ea65857d3c8b8b3c8136d0207d85ff37
Albert Yu <kbalbertyu@gmail.com>
2020-04-29 20:38:48 +0800

    Restart driver when start fails

--- src/main/java/cn/btimes/service/WebDriverManager.java
+++ src/main/java/cn/btimes/service/WebDriverManager.java
@@ -1,8 +1,13 @@
 package cn.btimes.service;
 
+import cn.btimes.source.Source;
 import com.amzass.enums.common.ConfigEnums.ChromeDriverVersion;
+import com.amzass.utils.common.ProcessCleaner;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,6 +16,7 @@ import java.util.concurrent.TimeUnit;
  */
 class WebDriverManager extends com.amzass.service.common.WebDriverManager {
 
+    private final Logger logger = LoggerFactory.getLogger(Source.class);
     private static final int FACTOR_TIMEOUT = 10;
 
     private void setTimeOut(long timeout, WebDriver driver) {
@@ -22,8 +28,16 @@ class WebDriverManager extends com.amzass.service.common.WebDriverManager {
 
     @Override
     public WebDriver initCustomChromeDriver(ChromeDriverVersion chromeDriverVersion, long timeout, DesiredCapabilities dCaps) {
-        WebDriver driver = super.initCustomChromeDriver(chromeDriverVersion, timeout, dCaps);
-        this.setTimeOut(timeout, driver);
+        WebDriver driver;
+        try {
+            driver = super.initCustomChromeDriver(chromeDriverVersion, timeout, dCaps);
+            this.setTimeOut(timeout, driver);
+        } catch (WebDriverException e) {
+            logger.error("Unable to start driver, restarting...", e);
+            ProcessCleaner.cleanWebDriver();
+            driver = super.initCustomChromeDriver(chromeDriverVersion, timeout, dCaps);
+            this.setTimeOut(timeout, driver);
+        }
         return driver;
     }
 }
