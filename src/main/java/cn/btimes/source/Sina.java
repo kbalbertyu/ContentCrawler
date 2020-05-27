aee5c5b965c8fef3756d6b37ec1d31189d2cf174
Albert Yu <kbalbertyu@gmail.com>
2020-05-27 14:50:02 +0800

    :bug: Fix Sina html cleaning syntax error

--- src/main/java/cn/btimes/source/Sina.java
+++ src/main/java/cn/btimes/source/Sina.java
@@ -124,7 +124,7 @@ public class Sina extends Source {
      */
     @Override
     protected String cleanHtml(Element dom) {
-        Elements elements = dom.select("[id^=ad], [class^=survey], [id^=quote_], script, .article-editor, p:contains(本文来自于), p:contains(原题为), p:contains(责任编辑), span[style*=KaiTi_GB2312], p:contains(来源：), p:contains(免责声明：), '.appendQr_wrap");
+        Elements elements = dom.select("[id^=ad], [class^=survey], [id^=quote_], script, .article-editor, p:contains(本文来自于), p:contains(原题为), p:contains(责任编辑), span[style*=KaiTi_GB2312], p:contains(来源：), p:contains(免责声明：), .appendQr_wrap");
         if (elements.size() > 0) {
             elements.remove();
         }
