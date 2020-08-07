da30ffdbea42e91caf6259e4e93514c99da84751
Albert Yu <kbalbertyu@gmail.com>
2020-08-07 21:00:27 +0800

    Add CBR21 article source

---
+++ src/main/java/cn/btimes/source/CBR21.java
@@ -0,0 +1,72 @@
+package cn.btimes.source;
+
+import cn.btimes.model.common.Article;
+import cn.btimes.model.common.CSSQuery;
+import cn.btimes.model.common.Category;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.WebDriver;
+
+import java.util.*;
+
+/**
+ * @author <a href="mailto:kbalbertyu@gmail.com">Albert Yu</a> 2020/8/7 20:11
+ */
+public class CBR21 extends Source {
+
+    private static final int MAX_PAST_DAYS = 2;
+    private static final Map<String, Category> URLS = new HashMap<>();
+
+    static {
+        URLS.put("http://www.21cbr.com/", Category.ECONOMY);
+    }
+
+    @Override
+    protected Map<String, Category> getUrls() {
+        return URLS;
+    }
+
+    @Override
+    protected String getDateRegex() {
+        return "\\d{4}-\\d{2}-\\d{2}";
+    }
+
+    @Override
+    protected String getDateFormat() {
+        return "yyyy-MM-dd";
+    }
+
+    @Override
+    protected CSSQuery getCSSQuery() {
+        return new CSSQuery("div.item", ".content", ".tit > a", "",
+            "", ".date");
+    }
+
+    @Override
+    protected int getSourceId() {
+        return 1873;
+    }
+
+    @Override
+    String getStatus() {
+        return "3";
+    }
+
+    @Override
+    protected Date parseDateText(String timeText) {
+        return this.parseDateTextWithDay(timeText, this.getDateRegex(), this.getDateFormat(), MAX_PAST_DAYS);
+    }
+
+    @Override
+    protected List<Article> parseList(Document doc) {
+        List<Article> articles = new ArrayList<>();
+        Elements list = this.readList(doc);
+        this.parseDateTitleList(articles, list);
+        return articles;
+    }
+
+    @Override
+    protected void readArticle(WebDriver driver, Article article) {
+        this.readContent(driver, article);
+    }
+}
