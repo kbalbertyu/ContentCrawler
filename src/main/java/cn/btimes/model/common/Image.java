4c7e86e0abe4e6a478b5d8bca68eabbbd08bc655
Albert Yu <kbalbertyu@gmail.com>
2020-09-16 10:52:02 +0800

    :bug: Fix JSON parsing error for Image

--- src/main/java/cn/btimes/model/common/Image.java
+++ src/main/java/cn/btimes/model/common/Image.java
@@ -2,12 +2,14 @@ package cn.btimes.model.common;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author <a href="mailto:kbalbertyu@gmail.com">Albert Yu</a> 2020/7/30 11:35
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Image {
     private String url;
     private String content;
